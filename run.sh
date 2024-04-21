91aab6d59af3adefd3b9ad69652aba4880559b52
JulianKennedy <julian.m.kennedy@gmail.com>
2024-04-21 02:52:15 -0700

    Middle embeddings again but with 120 filters and resnet 2

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ python pcnn_train.py \
 --sampling_interval 50 \
 --save_interval 50 \
 --dataset cpen455 \
---nr_resnet 1 \
---nr_filters 100 \
+--nr_resnet 2 \
+--nr_filters 120 \
 --nr_logistic_mix 5 \
 --lr_decay 0.999995 \
 --max_epochs 500 \
